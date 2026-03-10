e217d4985484a4e976c4b8ee8d60de8f16fde054
mohsen <gasimadam111@gmail.com>
2026-03-10 03:24:25 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
+import Privacy from './components/Privacy'
+import Terms from './components/Terms'
+import Support from './components/Support'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"         element={<Dashboard />} />
+        <Route path="/privacy"  element={<Privacy />} />
+        <Route path="/terms"    element={<Terms />} />
+        <Route path="/support"  element={<Support />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )

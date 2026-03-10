620a2fd679dbe49039e349bbc556457436f9d6d7
mohsen <gasimadam111@gmail.com>
2026-03-10 10:42:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
+    },
   },
-  esbuild: {
-    target: 'esnext',
-  }
 })
